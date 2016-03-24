4f0dea7f47a14f742cbb215746b6a2eec0692f1d
Break Yang <breakds@gmail.com>
2016-03-23 20:55:44 -0400

    change python indentation

--- emacs/.emacs.d/conf/python-dev.el
+++ emacs/.emacs.d/conf/python-dev.el
@@ -4,3 +4,8 @@
 
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i")
+
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 2))))
